07b1581eb3657cd22310ff20afaac5dc270091d3
Elliott Foster <elliottf@codebrews.com>
2016-11-04 08:56:28 -0500

    feat(s3): Allow additional s3 options
    serverless-builder will now accept options both for the object that
    is being sent to s3 as well as the s3 instance itself.

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const npm = require('npm');
 const tar = require('tar-fs');
 const temp = require('temp');
 const zlib = require('zlib');
-const S3 = require('aws-sdk').S3;
-const S3S = require('s3-streams');
+const { S3 } = require('aws-sdk');
+const { WriteStream } = require('s3-streams');
 
 temp.track();
 
@@ -16,13 +16,15 @@ temp.track();
  * @param {Object} packages
  *  An object of npm packages to install, keyed by package name, with the
  *  package version or install path as the value.
+ * @param {Object} s3ObjectOptions
+ *   Options to pass to the S3 write stream.
  * @param {Object} s3Options
- *   Options to pass to s3-streams.
+ *   Options to pass to the S3 instance.
  *
  * @return {Promise}
  *   Resolves when the install finishes, else rejects.
  */
-module.exports = (packages, s3Options) => new Promise((resolve, reject) => {
+module.exports = (packages, s3ObjectOptions, s3Options = {}) => new Promise((resolve, reject) => {
   temp.mkdir('builder', (err, path) => {
     if (err) {
       return reject(err);
@@ -50,10 +52,10 @@ module.exports = (packages, s3Options) => new Promise((resolve, reject) => {
             const Key = `${path.split('/').pop()}.tgz`;
             tar.pack(`${path}/node_modules`)
               .pipe(zlib.Gzip())
-              .pipe(S3S.WriteStream(new S3(), Object.assign({ Key }, s3Options)))
+              .pipe(WriteStream(new S3(s3Options), Object.assign({ Key }, s3ObjectOptions)))
               .on('finish', () => {
                 resolve({
-                  Bucket: s3Options.Bucket,
+                  Bucket: s3ObjectOptions.Bucket,
                   Key,
                 });
               })
